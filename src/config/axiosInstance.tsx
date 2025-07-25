735f437a29040941a6f9a11779c8f31f01a6ebb4
Shay_Batonya <127010513+ShaiBatonya@users.noreply.github.com>
2025-07-25 07:27:30 +0300

    Update base URL to https://api.starquest.app for production integration

--- src/config/axiosInstance.tsx
+++ src/config/axiosInstance.tsx
@@ -2,7 +2,7 @@ import axios, { AxiosInstance, AxiosError, InternalAxiosRequestConfig } from 'ax
 import { paths } from '@/routes/paths';
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: 'https://starquest.app',
+  baseURL: 'https://api.starquest.app',
   headers: {
     'Content-Type': 'application/json',
   },
